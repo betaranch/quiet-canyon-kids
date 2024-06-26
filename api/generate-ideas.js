1c9c86f01f88999eccfd580fb5f6f2a1c8c61736
betaranch <135581907+betaranch@users.noreply.github.com>
2024-06-26 13:41:51 -0700

    Update generate-ideas.js

--- api/generate-ideas.js
+++ api/generate-ideas.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
         content: "You are a helpful assistant that generates entrepreneurial ideas for children."
       }, {
         role: "user",
-        content: `Generate 10 creative entrepreneurial ideas for a ${age}-year-old child interested in ${interests}. Provide each idea as a short sentence.`
+        content: `Families are showing up to a neighborhood entreprenurial event with 10 other families and are looking for a fun and creative product they can sell to the neighbors that attend the event. All are to be sold between $1 - $5 dollars using single bills. Value of the products sold can slightly surpass sell price but hopefully not by much. Examples of items being sold by other families are bracelets, beach bags, lemonade, cookies. Generate 10 creative entrepreneurial product ideas for a ${age}-year-old child interested in ${interests}. Provide each idea as a short sentence without numbering or special characters. Have a second sentence with an estimate cost per unit and recommended sale price to make things easier on the parent`
       }],
       temperature: 0.7
     }, {
